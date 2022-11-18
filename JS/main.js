18f9b2e5c3da574b04ed1c520eabf657d31224f6
Azamat <105981809+sayfiddinov1off@users.noreply.github.com>
2022-11-18 15:18:38 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Ismingizni kiriting");
+let surname = prompt("Familyangizni kiritng");
+let age = prompt("Yoshingizni kiriting");
+let edu = prompt("O'qish joyingizni kiriting");
+
+console.log("Ism: " + firstName + ", Familya: " + surname + ", Yosh: " + age +", O'qish joyi " + edu);
